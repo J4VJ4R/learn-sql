eaf81a824923dc4fea9bd4dfada2831bdd0c36f2
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-12-03 03:06:37 -0500

    create from design a database

--- 03-design/blog.sql
+++ 03-design/blog.sql
@@ -1,24 +1,28 @@
 CREATE TABLE users (
     id          int(255) auto_increment not null,
-    name        varchar (100) not null,
-    lastname    varchar (100) not null,
-    email       varchar (255) not null,
-    password    varchar (255) not null,
+    name        varchar(100) not null,
+    lastname    varchar(100) not null,
+    email       varchar(255) not null,
+    password    varchar(255) not null,
     date        date not null,
-    CONSTRAINT pk_usuarios PRIMARY KEY (id)
-);
+    CONSTRAINT pk_users PRIMARY KEY (id),
+    CONSTRAINT uq_email UNIQUE(email)
+)ENGINE=InnoDb;
 
 CREATE TABLE categories (
     id      int(255) auto_increment not null,
-    name    varchar (100),
+    name    varchar(100),
     CONSTRAINT pk_categories PRIMARY KEY(id)
-);
+)ENGINE=InnoDb;
 
 CREATE TABLE entries (
     id              int(255) auto_increment not null,
-    user_id         int(255),
-    category_id     int(255),
-    title           varchar(255)
+    user_id         int(255) not null,
+    category_id     int(255) not null,
+    title           varchar(255) not null,
     description     MEDIUMTEXT,
-    date            date not null
-);
+    date            date not null,
+    CONSTRAINT pk_entries PRIMARY KEY(id),
+    CONSTRAINT fk_entry_user FOREIGN KEY(user_id) REFERENCES users(id),
+    CONSTRAINT fk_entry_category FOREIGN KEY(category_id) REFERENCES categories(id)
+)ENGINE=InnoDb;

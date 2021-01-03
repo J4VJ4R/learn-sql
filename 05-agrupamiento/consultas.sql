bc57269c94e722721615890a8ebaa14bdd3c57ec
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-01-03 18:43:13 -0500

    AVG, MAX, MIN AND SUM

--- 05-agrupamiento/consultas.sql
+++ 05-agrupamiento/consultas.sql
@@ -6,3 +6,17 @@ SELECT description, COUNT(description) AS 'QUANTITY', category_id FROM entries G
 
 SELECT COUNT(title) as 'NUMERO DE ENTRADAS', category_id
 FROM entries GROUP BY category_id HAVING COUNT(title) >= 2;
+
+/*
+    AVG => SACAR LA MEDIA
+    COUNT => CONTAR NÚMERO DE ELEMENTOS
+    MAX => VALOR MÁXIMO DEL GRUPO
+    MIN => VALOR MÍNIMO DEL GRUPO
+    SUM => SUMAR TODAS LAS EXPRESIONES DEL GRUPO O EL CONTENIDO
+ */
+
+ SELECT AVG(id) AS 'MEDIA DE ENTRADAS' FROM entries;
+
+ SELECT MAX(id) AS 'MAX OF ENTRIES' FROM entries;
+
+ SELECT SUM(id) AS 'MAX OF ENTRIES' FROM entries;

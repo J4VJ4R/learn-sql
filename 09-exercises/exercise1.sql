ea7a0b195d37004c50355f6d93bf533c9e747710
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-01-19 19:11:00 -0500

    create table sellers

--- 09-exercises/exercise1.sql
+++ 09-exercises/exercise1.sql
@@ -18,12 +18,18 @@ CREATE TABLE groups(
 
 CREATE TABLE sellers(
     id                  int (20) auto_increment not null,
-    group_id            
-    name
-    lastname
-    position
-    start_date
-    sales_comission
-    manager
+    group_id            int (20) not null,
+    name                varchar(100) not null,
+    lastname            varchar (100) not null,
+    position            varchar (100) not null,
+    start_date          date,
+    salary              int (20,2),
+    sales_comission     float(10,2),
+    manager             int (10),
+    CONSTRAINT pk_sellers PRIMARY KEY (id),
+    CONSTRAINT fk_seller_group FOREIGN KEY (group_id) REFERENCES groups(id),
+    CONSTRAINT fk_seller_manager FOREIGN KEY (manager) REFERENCES sellers(id)
 )ENGINE=InnoDB;
+
+
 
